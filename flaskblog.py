935cfc013e52f36858f35e7e98415f2179a64fc9
m-c-hill <matthewhill2595@gmail.com>
2021-10-08 16:56:49 +0100

    User and Post models

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'bf44d8000ecb0f94f01dcc7451318648' # TODO: Add to .secrets dir
-
 # TODO: add in commit validation to git (black, pylava)
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+from typing import Callable
+
+# Flask application configuration
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bf44d8000ecb0f94f01dcc7451318648' # TODO: Add to .secrets dir
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/site.db'
+
+
+class MySQLAlchemy(SQLAlchemy):
+    # Workaround for issues with IDE not recognising Flask SQLAlchemy methods: https://www.py4u.net/discuss/167613
+    Column: Callable
+    String: Callable
+    Integer: Callable
+    relationship: Callable
+    Text: Callable
+    DateTime: Callable
+    ForeignKey: Callable
+
+
+db = MySQLAlchemy(app)  # SQLAlchemy database instance created
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)  # Uppercase Post refers to class
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Lowercase user refers to table name
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}'"
 
 posts = [
     {
