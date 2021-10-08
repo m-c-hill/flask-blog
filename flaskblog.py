77ca08aaa4255e765c43369e3de243ef3ca782fa
m-c-hill <matthewhill2595@gmail.com>
2021-10-08 13:58:58 +0100

    about and home page, html layout, css/bootstrap styling

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+posts = [
+    {
+        'author': 'Matt Hill',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '2021-09-01'
+    },
+    {
+        'author': 'Matt Hill',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '2021-09-02'
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
